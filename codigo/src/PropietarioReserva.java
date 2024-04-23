99fef8869fc3b0c931125e425557cdd23a2e104d
Felipe Prudêncio <prudenciofpdc@gmail.com>
2024-04-23 12:29:13 -0300

    Update PropietarioReserva.java

--- codigo/src/PropietarioReserva.java
+++ codigo/src/PropietarioReserva.java
@@ -1,13 +1,78 @@
+import java.util.Date;
+
 public class PropietarioReserva {
     private String nome;
-    private String telefone;
-    private String cpf;
+    private int telefone;
     private int totalPessoas;
+    private int cpf;
 
-    public PropietarioReserva(String nome, String telefone, String cpf, int totalPessoas) {
+    // Construtor
+    public PropietarioReserva(String nome, int telefone, int totalPessoas, int cpf) {
         this.nome = nome;
         this.telefone = telefone;
-        this.cpf = cpf;
         this.totalPessoas = totalPessoas;
+        this.cpf = cpf;
+    }
+
+    // Método para realizar uma requisição
+    public void realizarRequisicao(int numPessoas) {
+        this.totalPessoas = numPessoas;
+        // Aqui vai a lógica para realizar uma requisição
+    }
+
+    // Método para entrar na fila de espera
+    public void entrarFilaEspera() {
+        // Aqui vai a lógica para entrar na fila de espera
+    }
+
+    // Método para registrar a entrada do cliente
+    public void registrarEntrada(Date dataEntrada) {
+        // Registra a data e hora atual como a entrada do cliente
+        Date dataEntrada = new Date();
+        System.out.println("Entrada registrada para " + this.nome + " em " + dataEntrada);
+    }
+
+    // Método para registrar a saída do cliente
+    public void registrarSaida(Date dataSaida) {
+        // Registra a data e hora atual como a entrada do cliente
+        Date dataEntrada = new Date();
+        System.out.println("Entrada registrada para " + this.nome + " em " + dataEntrada);
+    }
+
+    // Método para atualizar o nome do cliente
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    // Método para atualizar o telefone do cliente
+    public void setTelefone(int novoTelefone) {
+        this.telefone = novoTelefone;
+    }
+
+    // Método para atualizar o CPF do cliente
+    public void setCpf(int novoCpf) {
+        this.cpf = novoCpf;
+    }
+    
+    // Método para atualizar o total de pessoas na reserva do cliente
+    public void setTotalPessoas(int novoTotalPessoas) {
+        this.totalPessoas = novoTotalPessoas;
+    }
+
+    // Métodos de acesso (getters)
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getTelefone() {
+        return this.telefone;
+    }
+
+    public int getTotalPessoas() {
+        return this.totalPessoas;
+    }
+
+    public int getCpf() {
+        return this.cpf;
     }
 }
