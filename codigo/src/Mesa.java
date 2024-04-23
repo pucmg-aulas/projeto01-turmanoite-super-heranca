d8ace6631f57bc599612ef731765e946ebf95f61
Akino <67321539+Aquin0student@users.noreply.github.com>
2024-04-23 17:19:07 -0300

    Update Mesa_V2.java

--- codigo/src/Mesa.java
+++ codigo/src/Mesa.java
@@ -2,10 +2,20 @@ public class Mesa {
     private int capacidade;
     private boolean disponibilidade;
 
+
     public boolean verificarCapacidade(int totalDePessoas){
         return totalDePessoas <= capacidade;
     }
     public boolean verificarDisponibilidade(){
         return disponibilidade;
     }
+
+    public void ocuparMesa(){
+        disponibilidade = false;
+    }
+
+    public void desocuparMesa(){
+        disponibilidade = true;
+    }
+
 }
