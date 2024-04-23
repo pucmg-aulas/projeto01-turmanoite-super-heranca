92549989ea4833b1b463d021984ebf781d6bb419
Felipe Corrêa <105075783+FelipeDev92@users.noreply.github.com>
2024-04-23 10:32:49 -0300

    Update Reserva.java
    Adicionado o construtor Reserva

--- codigo/src/Reserva.java
+++ codigo/src/Reserva.java
@@ -6,6 +6,13 @@ public class Reserva {
     private PropietarioReserva propietarioReserva;
     private Mesa mesa;
 
+    public Reserva(LocalDateTime horaChegada, LocalDateTime horaSaida, PropietarioReserva propietarioReserva, Mesa mesa){
+        this.horaChegada = LocalDateTime;
+        this.horaSaida = LocalDateTime;
+        this.proprietarioReserva = proprietarioReserva;
+        this.mesa = mesa;
+    }
+
     public void fazerReserva(PropietarioReserva propietarioReserva, Mesa mesa) {
         this.horaChegada = LocalDateTime.now();
         this.propietarioReserva = propietarioReserva;
